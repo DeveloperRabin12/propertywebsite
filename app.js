7707be6f54a8cdccd5364117e06807509b2084c4
Rabin <rabing624@gmail.com>
2025-02-27 22:06:02 +0545

    complete all route

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const connectDB = require('./dbConnect');
 const Listing = require('./models/listingmodel')
+const path = require('path');
+const methodOverride = require('method-override');
+
+
+app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 //connecting to db from importing dbConnect.js
@@ -13,9 +22,62 @@ app.get('/', (req, res) => {
 
 app.get('/listings', async(req, res) => {
     const alllisting = await Listing.find({});
-    console.log(alllisting)
-    res.send("listing fetched")
+    res.render('listings/index.ejs', { alllisting });
 });
+
+app.get('/listings/new', (req, res) => {
+    res.render('listings/new.ejs');
+})
+
+app.get('/listings/:id', async(req, res) => {
+    let {id} = req.params;
+    const onelisting = await Listing.findById(id);
+    res.render('listings/show.ejs', { onelisting });
+
+
+})
+
+//create new listing
+app.post('/listings', async(req, res) => {
+    const { title, description, image, price, location, country } = req.body;
+    let newListing = new Listing({
+        title,
+        description,
+        image,
+        price,
+        location,
+        country
+    })
+    await newListing.save();
+    res.redirect('/listings')
+})
+
+
+//edit listing
+app.get('/listings/:id/edit', async(req, res) => {
+    let {id} = req.params;
+    const onelisting = await Listing.findById(id);
+    res.render('listings/edit.ejs', { onelisting });
+    console.log("page loaded")
+})
+
+//edited listing
+app.put('/listings/:id', async(req, res) => {
+    let {id} = req.params;
+    const { title, description, image, price, location, country } = req.body;
+    await Listing.findByIdAndUpdate(id, { title, description, image, price, location, country });
+    console.log("updated")
+    res.redirect(`/listings`);
+});
+
+//delete listing
+app.delete('/listings/:id', async(req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    
+    res.redirect('/listings');
+});
+
 
 
 // app.get('/testlisting', async(req, res) => {
@@ -34,3 +96,4 @@ app.get('/listings', async(req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
+
